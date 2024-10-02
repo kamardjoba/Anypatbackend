2bd8bd4d7f8f61435003482d9290718ed6398acf
kamardjoba <Nazarlymar152@gmail.com>
2024-10-02 17:35:49 +0200

    initial

--- index.js
+++ index.js
@@ -204,7 +204,7 @@ mongoose.connect(MONGODB_URL,)
 
 
     app.get('/user-info', async (req, res) => {
-        const { telegramId1 } = req.query;
+        const { telegramId } = req.query;
     
         try {
             let user = await UserProgress.findOne({ telegramId });
